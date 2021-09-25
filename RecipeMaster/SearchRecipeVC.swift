5dd29262f4e5c5a0e909ce6be3e7dd98c027da13
sudhakar <sudhakarvarma.ios@gmail.com>
2021-09-25 21:46:01 +0530

    commit

--- RecipeMaster/SearchRecipeVC.swift
+++ RecipeMaster/SearchRecipeVC.swift
@@ -40,9 +40,7 @@ class SearchRecipeVC: UIViewController,UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        print("This is master")
-        
+                        
         listingTV.tableFooterView = UIView()
         
         listingTV.estimatedRowHeight = 247
